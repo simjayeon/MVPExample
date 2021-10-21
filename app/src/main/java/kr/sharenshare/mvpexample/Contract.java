b3024f3cc7b5b678ce1cdaf65e80ab17de4ace64
simjayeon <simjayeon98@gmail.com>
2021-10-21 11:36:07 +0900

    주석

--- app/src/main/java/kr/sharenshare/mvpexample/Contract.java
+++ app/src/main/java/kr/sharenshare/mvpexample/Contract.java
@@ -3,6 +3,8 @@ package kr.sharenshare.mvpexample;
 import org.json.JSONObject;
 
 //View와 Presenter의 Interface
+//View와 Presenter가 무슨 기능을 할지 미리 명세해주는 역할
+//작성 후 implement 하여 사용
 public interface Contract {
     //결과를 보여주는 일
     interface View{
